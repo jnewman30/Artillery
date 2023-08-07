21e063331f0a76b132205025619e4f77d3c89d9e
Jeremy Newman <jnewman30@gmail.com>
2023-08-06 22:30:39 -0500

    Check collisions

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -1,6 +1,5 @@
 extends RigidBody2D
 
-
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 #	pass # Replace with function body.
@@ -9,3 +8,21 @@ extends RigidBody2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _physics_process(delta):
+	pass
+
+
+func _on_body_shape_entered(body_rid, body: PhysicsBody2D, body_shape_index, local_shape_index):
+	var body_groups = body.get_groups()
+	
+	if body_groups.find("Terrain") >= 0:
+		print_debug("Terrain Collision!")
+	
+	if body_groups.find("Vehichles") >= 0:
+		print_debug("Vehichle Collision!")
+	
+#	print_debug("collision body: ", body)
+#	print_debug("groups: ", body_groups)
+	queue_free()
